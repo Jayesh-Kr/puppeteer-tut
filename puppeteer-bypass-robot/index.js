a61a9072f951566fac0aed49f1519e74caba89ad
Jayesh Krishna <jayeshkr152005@gmail.com>
2025-02-16 15:08:37 +0530

    Test3 & Test4

--- puppeteer-bypass-robot/index.js
+++ puppeteer-bypass-robot/index.js
@@ -3,23 +3,22 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker'
 
 // Test1 --> Without StealthPlugin
+// Test2 --> With StealthPlugin
+puppeteer.use(StealthPlugin())
+puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
 
-// puppeteer.use(StealthPlugin())
-// puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
-
-puppeteer.launch({ headless: true }).then(async browser => {
+puppeteer.launch({ 
+    headless: true,
+    executablePath : "C:/Program Files/Google/Chrome/Application/chrome.exe",
+    userDataDir : "C:/Users/HP/AppData/Local/Google/Chrome/User Data"
+ }).then(async browser => {
   const page = await browser.newPage()
-  await page.setViewport({ width: 800, height: 600 })
-
-  console.log(`Testing adblocker plugin..`)
-  await page.goto('https://www.vanityfair.com')
-//   await page.waitForTimeout(1000)
-  await page.screenshot({ path: 't1_adblocker.png', fullPage: true })
+//   await page.setViewport({ width: 800, height: 600 })
 
   console.log(`Testing the stealth plugin..`)
   await page.goto('https://bot.sannysoft.com')
 //   await page.waitForTimeout(5000)
-  await page.screenshot({ path: 't1_stealth.png', fullPage: true })
+  await page.screenshot({ path: 't4_stealth.png', fullPage: true })
 
   console.log(`All done, check the screenshots. ✨`)
   await browser.close()
